688430c50cdd8a510574d8f98caa2a6098c90b89
Rich Lewis <rich.lewis@oracle.com>
2018-04-26 16:53:46 -0500

    Updates

--- vagrant-shell/provision.sh
+++ vagrant-shell/provision.sh
@@ -2,36 +2,41 @@
 
 # Enable provisioning of the client with a shell script.
 echo "Entering Shell Provisoning"
-/bin/rm /etc/yum.repos.d/epel.repo /etc/yum.repos.d/epel-testing.repo
+
 yum makecache fast
-yum install deltarpm -y
-yum install wget -y
+yum install deltarpm --assumeyes
+yum install wget --assumeyes
 
 /bin/mv /etc/yum.repos.d/public-yum-ol7.repo /etc/yum.repos.d/public-yum-ol7.repo.orig
 wget -q http://yum.oracle.com/public-yum-ol7.repo -P /etc/yum.repos.d
 
-yum install yum-utils -y
+yum install yum-utils --assumeyes
+yum-config-manager --enable ol7_latest
+yum-config-manager --enable ol7_UEKR4
+yum-config-manager --enable ol7_optional_latest
 yum-config-manager --enable ol7_addons
+#yum-config-manager --enable ol7_UEKR4_OFED
 yum-config-manager --enable ol7_software_collections
 yum-config-manager --enable ol7_developer
 yum-config-manager --enable ol7_developer_EPEL
-
-yum makecache fast
+#yum-config-manager --enable ol7_openstack40
+#yum-config-manager --enable ol7_openstack40_extras
+#yum-config-manager --enable ol7_MODRHCK
+#yum-config-manager --enable ol7_preview
 
 yum install dos2unix -y
 yum install terraform -y
+yum install terraform-provider-oci -y
 
-cd /tmp
-wget -q https://github.com/oracle/terraform-provider-oci/releases/download/v2.0.1/linux.tar.gz -O /tmp/linux.tar.gz
-tar xf /tmp/linux.tar.gz
-sudo cp /tmp/linux_amd64/terraform-provider-oci_v2.0.1 /usr/bin/terraform-provider-oci
+setenforce Permissive
+sed -i 's/=enforcing/=permissive/g' /etc/selinux/config /etc/selinux/config
+
+yum makecache fast
 
 echo " "
 ip addr show
 
-
 date +"%F %T" > /etc/git-creation-timestamp.txt
-echo " "
 cat /etc/git-creation-timestamp.txt
-echo " "
 echo "Exiting Shell Provisoning"
+echo " "
